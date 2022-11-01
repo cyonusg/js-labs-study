638cddff3df7f2ada9ff0831448aa9c35a2998be
Carlos Yonusg <cyonusgdev@gmail.com>
2022-11-01 02:02:09 -0300

    labs tablas hash]

---
+++ tableHash/index.js
@@ -0,0 +1,9 @@
+const hash = (key_to_hash) => {
+    const hash_key_index = "";
+
+    return hash_key_index;
+}
+
+let james_index = hash("james");
+let mary_index = hash("mary");
+let steven_index = hash("steven");
